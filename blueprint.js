533eee3b7aa05a2891b13f3d0632315e8f5e121b
GauHelldragon <kylejackson6.kj@gmail.com>
2017-01-27 11:36:19 -0800

    Ajax Attempt

--- blueprint.js
+++ blueprint.js
@@ -690,10 +690,11 @@ function getOutput( dict, blueprint ) {
 
 $(document).ready(function(){
 	$( "#run" ).click(function() { 
-		dictionary = $.parseJSON(dictString);
+		//dictionary = $.parseJSON(dictString);
+		$.getJSON( "http://GauHelldragon.github.io/ftdDB.json", function( dictionary ) {
 		var blueprint = $( "#blueprint" ).val();
 		$("#output").html( getOutput(dictionary,blueprint) );
-			
+		})
 		
 	})
 })
